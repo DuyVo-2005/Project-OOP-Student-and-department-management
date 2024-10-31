0a9ef4bf3d4bc65071f8d80ea92b0b14f48b3834
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2024-10-31 19:47:58 +0700

    Add files via upload

---
+++ li thuyet.cpp
@@ -0,0 +1,33 @@
+﻿// li thuyet.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include<iostream>
+using namespace std;
+//quan he bao ham:
+	//- compesition(thanh phan): hinh thoi đặc huong ve class bao ham, hủy hàm cha -> hủy luôn hàm con (dùng thông thường)
+	//- aggregation(ket hop): hinh thoi rỗng (trăng) huong ve class bao ham, hủy hàm cha -> hàm con không hủy
+class B {
+public:
+	B() {
+		cout << "\nTao B";
+	};
+	~B() {
+		cout << "\nHuy B";
+	};
+};
+class A {
+public:
+	int x = 5;
+	B* b;
+	A() {
+		cout << "\nTao A";
+		b = new B();// nen dat ben trong de de quan ly ->tranh ro bo nho
+	}
+	~A() {
+		cout << "\nHuy A";
+		//delete b;//neu muon huy ca b -> theo quan he component (huy 1 class -> huy luon class chua no) (quan he thanh phan) -> hinh thoi dac
+	}
+};
+int main(int argc, char* argv[])
+{
+	A a;
+	return 0;
+}
